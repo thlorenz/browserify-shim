3c48b734c4750525ae308370fd8668b60a719787
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-20 12:37:21 -0500

    inline shims without depends parsed and resolved correctly

--- test/resolve-shims.js
+++ test/resolve-shims.js
@@ -8,7 +8,7 @@ function inspect(obj, depth) {
   console.error(require('util').inspect(obj, false, depth || 5, true));
 }
 
-test('\nno dependencies, external shim file, no redirect', function (t) {
+test('\nno dependencies, external shim file, no expose', function (t) {
   resolve(require.resolve('./nodeps/extshim/vendor/non-cjs'), function (err, res) {
     if (err) return t.fail(err);
     t.deepEqual(res, { exports: 'noncjs', depends: undefined }, 'resolves noncjs shim correctly')
@@ -18,6 +18,22 @@ test('\nno dependencies, external shim file, no redirect', function (t) {
 
 test('\nno dependencies, external shim, exposed as non-cjs', function (t) {
   resolve(require.resolve('./nodeps/extshim-exposed/vendor/non-cjs'), function (err, res) {
+    if (err) return t.fail(err);
+    t.deepEqual(res, { exports: 'noncjs', depends: undefined }, 'resolves noncjs shim correctly')
+    t.end();
+  });
+})
+
+test('\nno dependencies, inline shims, no expose', function (t) {
+  resolve(require.resolve('./nodeps/inlineshim/vendor/non-cjs'), function (err, res) {
+    if (err) return t.fail(err);
+    t.deepEqual(res, { exports: 'noncjs', depends: undefined }, 'resolves noncjs shim correctly')
+    t.end();
+  });
+})
+
+test('\nno dependencies, inline shims, exposed as non-cjs', function (t) {
+  resolve(require.resolve('./nodeps/inlineshim-exposed/vendor/non-cjs'), function (err, res) {
     if (err) return t.fail(err);
     t.deepEqual(res, { exports: 'noncjs', depends: undefined }, 'resolves noncjs shim correctly')
     t.end();
