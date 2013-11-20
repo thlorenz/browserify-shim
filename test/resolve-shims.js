4cf5f23e81dcd3d4fed8dd2c3e25da2b6f3e0afc
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-20 10:11:16 -0500

    test for resolving nodeps - ext shim

--- test/resolve-shims.js
+++ test/resolve-shims.js
@@ -8,10 +8,11 @@ function inspect(obj, depth) {
   console.error(require('util').inspect(obj, false, depth || 5, true));
 }
 
-test('\n', function (t) {
-  resolve(require.resolve('./nodeps-extshim/vendor/non-cjs'), function (err, res) {
+test('\nno dependencies, external shim file', function (t) {
+  resolve(require.resolve('./nodeps/extshim/vendor/non-cjs'), function (err, res) {
     if (err) return t.fail(err);
-    inspect(res);
+    t.deepEqual(res, { exports: 'noncjs', depends: undefined }, 'resolves noncjs shim correctly')
     t.end();
   });
 })
+
