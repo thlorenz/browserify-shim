c0a561ed37d89afc6cb1b0be4b268b347b629270
Thorsten Lorenz <thlorenz@gmx.de>
2014-02-24 15:10:07 -0500

    removing old bower fix, have failing test to show issue
    - test was updated to make copy of fixture since browserify-shim will remove files

--- test/bundle-pack-bower.js
+++ test/bundle-pack-bower.js
@@ -3,28 +3,40 @@
 
 var browserify = require('browserify')
   , test = require('tap').test
+  , path = require('path')
   , vm = require('vm')
+  , ncp = require('ncp')
+  , rmrf = require('rimraf')
 
 function bundleNcheck(relPath, t) {
-  browserify( { ignoreGlobals: true })
-    .require(require.resolve(relPath))
-    .bundle(function (err, src) {
-      if (err) { t.fail(err); return t.end(); }
+  // the @fixPackages feature actually removes package.json files from the file system, so we need to make a copy for this test
+  var relPathCpy = relPath + '-copy';
+  ncp(path.join(__dirname, relPath), path.join(__dirname, relPathCpy), function (err) {
+    if (err) { t.fail(err); return t.end(); }
+    
+    browserify( { ignoreGlobals: true })
+      .require(require.resolve(relPath))
+      .bundle(function (err, src) {
+        rmrf.sync(path.join(__dirname, relPathCpy));
 
-      var ctx = { window: {}, console: console };
-      ctx.self = ctx.window;
-      var require_ = vm.runInNewContext(src, ctx);
-       
-      var main = require_(require.resolve(relPath));
+        if (err) { t.fail(err); return t.end(); }
 
-      t.deepEqual(
-          main
-        , { root: '$.position', fn: '$.fn.position', ui: '$.ui.position' }
-        , 'shims it correctly including requiring its dependends'
-      );
-      t.end()
-    });
+        var ctx = { window: {}, console: console };
+        ctx.self = ctx.window;
+        var require_ = vm.runInNewContext(src, ctx);
+        
+        var main = require_(require.resolve(relPath));
+
+        t.deepEqual(
+            main
+          , { root: '$.position', fn: '$.fn.position', ui: '$.ui.position' }
+          , 'shims it correctly including requiring its dependends'
+        );
+        t.end()
+      });
+  });
 }
+
 
 test('\nbundling pack that has shimmed bower component dependency'
     , bundleNcheck.bind(null, './bower'))
--- test/resolve-shims-bower.js
+++ test/resolve-shims-bower.js
@@ -10,10 +10,12 @@ function inspect(obj, depth) {
   console.error(require('util').inspect(obj, false, depth || 5, true));
 }
 
-test('\nresolving a bower component that is shimmed', function (t) {
+// TODO: may not be needed since bundle test proves that it works and the @fixPackage feature gets activated once
+//       for browserify-shim itself, so package.json files would be removed at this point
+/*test('\nresolving a bower component that is shimmed', function (t) {
   resolve(require.resolve('./bower/components/jquery-ui/ui/jquery.ui.position.js'), msgs, function (err, res) {
     if (err) { t.fail(err); return t.end(); }
     t.deepEqual(res.shim, { exports: null, depends: { jquery: 'jQuery' } }, 'resolves shim correctly')
     t.end();
   });
-})
+})*/
