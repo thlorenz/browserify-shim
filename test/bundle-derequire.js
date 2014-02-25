2a780500367a3eba579cede53b5478a5229a7ba8
Thorsten Lorenz <thlorenz@gmx.de>
2014-02-25 11:32:36 -0500

    reproducing invalid require issue with failing test

---
+++ test/bundle-derequire.js
@@ -0,0 +1,29 @@
+'use strict';
+/*jshint asi: true */
+
+var browserify = require('browserify')
+  , test = require('tap').test
+  , vm = require('vm')
+
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true));
+}
+
+function bundleNcheck(relPath, t) {
+  browserify( { ignoreGlobals: true })
+    .require(require.resolve(relPath))
+    .bundle(function (err, src) {
+      if (err) { t.fail(err); return t.end(); }
+
+      var ctx = { window: {}, console: console };
+      ctx.self = ctx.window;
+      var require_ = vm.runInNewContext(src, ctx);
+       
+      var main = require_(require.resolve(relPath));
+      t.equal(main, 'I survived the horror of humans messing with my brain', 'shims that module just fine');
+      t.end()
+    });
+}
+
+test('\nshimming a module that has an invalid require that needs to be derequired'
+    , bundleNcheck.bind(null, './derequire/main'))
---
+++ test/derequire/main.js
@@ -0,0 +1,13 @@
+// Some modules require other modules that they haven't even installed (trying to be commonJS compliant the wrong way)
+// A more detailed case can be found inside bower-ember
+
+var jQuery;
+
+// we will need to reassign the require (after we did our require calls to get depends)
+// therefore we'll add `require = undefined` above the code of this module
+if (!jQuery && typeof require === 'function') {
+  // browserify will try to find 'jQuery' now, so we have to derequire it
+  jQuery = require('jquery');
+}
+
+module.exports = 'I survived the horror of humans messing with my brain';
