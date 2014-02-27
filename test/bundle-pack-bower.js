d5d5755da50aad843278fcaa96a0fb24617b657c
Thorsten Lorenz <thlorenz@gmx.de>
2014-02-27 11:26:47 -0500

    adjust bower test since no longer fixing packages for bower

--- test/bundle-pack-bower.js
+++ test/bundle-pack-bower.js
@@ -7,36 +7,26 @@ var browserify = require('browserify')
   , test = require('tap').test
   , path = require('path')
   , vm = require('vm')
-  , ncp = require('ncp')
-  , rmrf = require('rimraf')
 
 function bundleNcheck(relPath, t) {
-  // the @fixPackages feature actually removes package.json files from the file system, so we need to make a copy for this test
-  var relPathCpy = relPath + '-copy';
-  ncp(path.join(__dirname, relPath), path.join(__dirname, relPathCpy), function (err) {
-    if (err) { t.fail(err); return t.end(); }
-    
-    browserify( { ignoreGlobals: true })
-      .require(require.resolve(relPathCpy))
-      .bundle(function (err, src) {
-        rmrf.sync(path.join(__dirname, relPathCpy));
+  browserify( { ignoreGlobals: true })
+    .require(require.resolve(relPath))
+    .bundle(function (err, src) {
+      if (err) { t.fail(err); return t.end(); }
 
-        if (err) { t.fail(err); return t.end(); }
+      var ctx = { window: {}, console: console };
+      ctx.self = ctx.window;
+      var require_ = vm.runInNewContext(src, ctx);
+      
+      var main = require_(require.resolve(relPath));
 
-        var ctx = { window: {}, console: console };
-        ctx.self = ctx.window;
-        var require_ = vm.runInNewContext(src, ctx);
-        
-        var main = require_(require.resolve(relPathCpy));
-
-        t.deepEqual(
-            main
-          , { root: '$.position', fn: '$.fn.position', ui: '$.ui.position' }
-          , 'shims it correctly including requiring its dependends'
-        );
-        t.end()
-      });
-  });
+      t.deepEqual(
+          main
+        , { root: '$.position', fn: '$.fn.position', ui: '$.ui.position' }
+        , 'shims it correctly including requiring its dependends'
+      );
+      t.end()
+    });
 }
 
 
